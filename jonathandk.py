e0ef9228eb94766c76eaf31767e09875ff975631
Jonathan de Kock <jonathan@wapp.co.za>
2020-04-14 16:34:24 +0200

    Add Jonathan de Kock's Into

---
+++ jonathandk.py
@@ -0,0 +1,4 @@
+print('===================================================')
+print('Hello DSC! I am Jonathan de Kock')
+print('I am a fullstack web developer and game hobbyist!')
+print('===================================================')
